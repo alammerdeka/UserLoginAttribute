bf52d3018ab6842bfc709f77d3e13e26a488a580
Bayu Alamsyah <45076273+alammerdeka@users.noreply.github.com>
2021-10-08 18:57:10 +0700

    Update app_url.dart

--- app_url.dart
+++ app_url.dart
@@ -1,6 +1,6 @@
 class AppUrl {
 
-  static const String baseUrl = 'http://10.0.2.2:4000/api/v1';
+  static const String baseUrl = 'url';
 
   static const String login = baseUrl + '/ApiLogin';
   static const String register = baseUrl + '/registration';
