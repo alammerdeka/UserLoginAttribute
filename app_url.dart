bf9011f094d80e17ad4f52e6008f39e0b758ea8f
Bayu Alamsyah <45076273+alammerdeka@users.noreply.github.com>
2021-10-09 21:32:06 +0700

    Update app_url.dart

--- app_url.dart
+++ app_url.dart
@@ -1,8 +1,8 @@
 class AppUrl {
 
-  static const String baseUrl = 'url';
+  static const String baseUrl = 'http://192.168.43.32/flagship/api/';
 
-  static const String login = baseUrl + '/ApiLogin';
+  static const String login = baseUrl + '/Auth';
   static const String register = baseUrl + '/registration';
   static const String forgotPassword = baseUrl + '/forgot_password';
 }
